df05a662ccffe6c470496c7413e1de51bffcb314
CheeseburgerIM <13051172893@163.com>
2023-02-03 15:28:20 +0800

    Site updated: 2023-02-03 15:28:20

--- script.js
+++ script.js
@@ -92,3 +92,19 @@ window.onload = function () {
 function Jarvis() {
     console.log('Jarvis is my co-pilot')
 }
+
+(function () {
+    var ua = window.navigator.userAgent,
+        agents = ['Android', 'iPhone', 'SymbianOS', 'Windows Phone', 'iPod'],
+        isPC = true
+    if (/\sVR\s/g.test(ua)) return
+    for (var i = 0, len = agents.length; i < len; i++) {
+        if (ua.indexOf(agents[i]) > 0) {
+            isPC = false
+            break
+        }
+    }
+    if (!isPC) {
+        window.location.href = 'https://m.panyizhe.com'
+    }
+})()
