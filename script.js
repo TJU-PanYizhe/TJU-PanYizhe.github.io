fe90fc8ab5f463970b2bb9926694950dafb08a15
CheeseburgerIM <13051172893@163.com>
2023-04-08 14:21:25 +0800

    Site updated: 2023-04-08 14:21:24

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ function Jarvis() {
 }
 
 // 客户端跳转
-// (function () {
-//     var ua = window.navigator.userAgent,
-//         agents = ['Android', 'iPhone', 'SymbianOS', 'Windows Phone', 'iPod', 'iPad'],
-//         isPC = true
-//     if (/\sVR\s/g.test(ua)) return
-//     for (var i = 0, len = agents.length; i < len; i++) {
-//         if (ua.indexOf(agents[i]) > 0) {
-//             isPC = false
-//             break
-//         }
-//     }
-//     if (!isPC) {
-//         window.location.href = 'https://m.panyizhe.com'
-//     }
-// })();
+(function () {
+    var ua = window.navigator.userAgent,
+        agents = ['Android', 'iPhone', 'SymbianOS', 'Windows Phone', 'iPod', 'iPad'],
+        isPC = true
+    if (/\sVR\s/g.test(ua)) return
+    for (var i = 0, len = agents.length; i < len; i++) {
+        if (ua.indexOf(agents[i]) > 0) {
+            isPC = false
+            break
+        }
+    }
+    if (!isPC) {
+        window.location.href = 'https://m.panyizhe.com'
+    }
+})();
 
 // 输出客户端信息
 (function () {
