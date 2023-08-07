31577f70b0ed95868fccefd6cc05e527cbb8f515
CheeseburgerIM <13051172893@163.com>
2023-08-07 14:45:21 +0800

    Site updated: 2023-08-07 14:45:21

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
 console.log('Vue3: ', 'https://cn.vuejs.org/')
-console.log('ElementPlus: ', 'https://element-plus.org/')
-
-// Jarvis
-function Jarvis() {
-    console.log('Jarvis is my co-pilot')
-}
+console.log('ElementPlus: ', 'https://element-plus.gitee.io/zh-CN/')
