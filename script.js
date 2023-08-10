8b80a8854497643fd2e6c2d66cd6ab5f3268dfc0
CheeseburgerIM <13051172893@163.com>
2023-08-10 14:25:47 +0800

    Site updated: 2023-08-10 14:25:47

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 console.log('Vue3: ', 'https://cn.vuejs.org/')
-console.log('ElementPlus: ', 'https://element-plus.gitee.io/zh-CN/')
+console.log('ElementPlus: ', 'https://element-plus.org/zh-CN/')
+console.log('若加载较慢可以访问: ', 'https://panyizhe.gitee.io')
+
+// 使用http协议
+if (location.protocol.startsWith('https')) {
+    location.replace(location.origin.replace('https', 'http'))
+}
