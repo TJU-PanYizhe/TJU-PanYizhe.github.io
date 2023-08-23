52361b274fcd5b14fc2e6f822fb58696a8baab7a
CheeseburgerIM <13051172893@163.com>
2023-08-23 16:44:23 +0800

    Site updated: 2023-08-23 16:44:23

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // 使用http协议
-if (location.protocol.startsWith('https')) {
-    location.replace(location.origin.replace('https', 'http'))
-}
+// if (location.protocol.startsWith('https')) {
+//     location.replace(location.origin.replace('https', 'http'))
+// }
 // 控制台输出信息
 window.onload = () => {
     setTimeout(() => {
