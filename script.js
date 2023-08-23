097cd07de2964d966a25a9828ace930573803c86
CheeseburgerIM <13051172893@163.com>
2023-08-23 16:35:23 +0800

    Site updated: 2023-08-23 16:35:23

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// 使用http协议
+if (location.protocol.startsWith('https')) {
+    location.replace(location.origin.replace('https', 'http'))
+}
 // 控制台输出信息
 window.onload = () => {
     setTimeout(() => {
         console.clear()
         console.log('Vue3: ', 'https://cn.vuejs.org/')
         console.log('ElementPlus: ', 'https://element-plus.gitee.io/zh-CN/')
-    }, 300)
+    }, 728)
 }
