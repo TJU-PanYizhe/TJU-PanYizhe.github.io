b82f3941e7ca11ed428f9572127372e5b17d02de
CheeseburgerIM <13051172893@163.com>
2023-09-11 23:51:57 +0800

    Site updated: 2023-09-11 23:51:56

--- script.js
+++ script.js
@@ -2,11 +2,30 @@
 // if (location.protocol.startsWith('https')) {
 //     location.replace(location.origin.replace('https', 'http'))
 // }
+function WASM() {
+    console.log(WebAssembly);
+    function fetchAndInstantiate(url, importObject) {
+        return fetch(url)
+            .then((response) => response.arrayBuffer())
+            .then((bytes) => WebAssembly.instantiate(bytes, importObject))
+            .then((results) => results.instance);
+    }
+
+    fetchAndInstantiate("./wasm/release.wasm", {})
+        .then(instance => {
+            const exports = instance.exports
+            console.log(exports);
+            // console.log(exports.add(1, 2));
+            // console.log(exports.add(-1, 0));
+            // console.log(exports.minus(10, 3));
+        })
+}
 // 控制台输出信息
 window.onload = () => {
     setTimeout(() => {
         console.clear()
         console.log('Vue3: ', 'https://cn.vuejs.org/')
         console.log('ElementPlus: ', 'https://element-plus.gitee.io/zh-CN/')
+        WASM()
     }, 728)
 }
