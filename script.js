0ba19a219c6b3380e407dd236c5d0cc988501e8c
CheeseburgerIM <13051172893@163.com>
2023-12-10 20:32:32 +0800

    2023/12/10 PYZcom

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// 使用http协议
-// if (location.protocol.startsWith('https')) {
-//     location.replace(location.origin.replace('https', 'http'))
-// }
 // WebAssembly Test
 function WASM() {
     // console.log(WebAssembly);
@@ -25,7 +21,7 @@ function WASM() {
 // 控制台输出信息
 window.onload = () => {
     setTimeout(() => {
-        console.clear()
+        // console.clear()
         console.log('Vue3: ', 'https://cn.vuejs.org/')
         console.log('ElementPlus: ', 'https://element-plus.gitee.io/zh-CN/')
         WASM()
