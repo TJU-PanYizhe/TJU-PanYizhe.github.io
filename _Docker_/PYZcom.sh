cfae9d0b8c0bd5c61e878a12aafa5caa073c4a47
CheeseburgerIM <13051172893@163.com>
2024-01-07 21:50:43 +0800

    2024/01/07 PYZcom

--- _Docker_/PYZcom.sh
+++
@@ -1,9 +0,0 @@
-# 1. 克隆仓库
-git clone https://gitee.com/PanYizhe/PanYizhe.git
-# 2. 构建镜像
-cd ./PanYizhe/_Docker_
-sudo docker build -f PYZcom.Dockerfile -t pyzcom:latest .
-sudo docker images pyzcom
-# 3. 启动容器
-sudo docker run -it --name pyzcom-deploy -p 80:80 -p 443:443 pyzcom /bin/bash
-# 4. 访问测试
